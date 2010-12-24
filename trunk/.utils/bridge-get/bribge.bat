6253427a19d3551cfb611b31a6068e455a0ca8a8
baden.i.ua@gmail.com <baden.i.ua@gmail.com@72927f16-9f08-a1fb-f96f-45abbbfd52dd>
2010-12-24 15:02:25 +0000

    Config base.

--- trunk/.utils/bridge-get/bribge.bat
+++ trunk/.utils/bridge-get/bribge.bat
@@ -9,32 +9,26 @@ echo .
 echo .
 echo .
 
-echo Opel-Omega
-%PY_PATH% bridge.py 356895035359317
-
-goto :EOF
-
-echo GPS1
-%PY_PATH% bridge.py 356895035376246
-
-
-echo GPS2
-%PY_PATH% bridge.py 356895035358996
-
-
-echo GPS6
-%PY_PATH% bridge.py 359587015340758
-
-
-echo GPS7
-%PY_PATH% bridge.py 359587017100622
-
+%PY_PATH% bridge.py 359587015371480 Львов-2
+%PY_PATH% bridge.py 353358017117115 GPS-8
+%PY_PATH% bridge.py 359587015328522 Володя-11
+%PY_PATH% bridge.py 359587017100622 GPS-7
+%PY_PATH% bridge.py 359587015340758 Львов-6
+%PY_PATH% bridge.py 356895035360612 Львов-5
+%PY_PATH% bridge.py 359587015565669 Львов-4
+%PY_PATH% bridge.py 353358019726996 Отладочная система
+%PY_PATH% bridge.py 356895035358996 GPS-2
+%PY_PATH% bridge.py 356895035376246 GPS-1
+%PY_PATH% bridge.py 356895035359317 Omega-Caravan AE1829BE
+%PY_PATH% bridge.py 353358016204856 Злагода-3
 
 echo ===================================================
 echo ^|^|  Для остановки синхнонизации нажмите Ctrl-C   ^|^|
 echo ===================================================
 rem D:\Programs\mingw\bin\sleep.exe 60
 
-%PY_PATH% pause.py
+rem %PY_PATH% pause.py
 
-goto loop
+rem goto loop
+
+
