b28ffc850356ae6d9218a9cbaf67b97b31deaf9b
baden.i.ua@gmail.com <baden.i.ua@gmail.com@72927f16-9f08-a1fb-f96f-45abbbfd52dd>
2011-02-05 13:54:21 +0000

    Prepare to google closure compiller.

--- js/.compiller/get.externals.bat
+++ js/.compiller/get.externals.bat
@@ -1,5 +1,11 @@
 SET curl=D:\Programs\Git\bin\curl.exe
 SET svn=D:\Programs\mingw\svn-win32-1.6.2\bin\svn.exe
 
-%curl% http://closure-compiler.googlecode.com/files/compiler-latest.zip --output compiler.jar
-%svn% checkout http://closure-compiler.googlecode.com/svn/trunk/contrib/externs externals
+rem %curl% http://closure-compiler.googlecode.com/files/compiler-latest.zip --output compiler.jar
+
+rem jquery-1.4.4.js
+for %%i in (jquery-1.5.js, google_loader_api.js, webkit_console.js, maps/google_maps_api_v3_3.js) do (
+%curl% http://closure-compiler.googlecode.com/svn/trunk/contrib/externs/%%i --output externals\%%i
+)
+
+rem %svn% checkout http://closure-compiler.googlecode.com/svn/trunk/contrib/externs externals
