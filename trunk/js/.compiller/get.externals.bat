e309bd2451c27517a763f6dc3c565a194e096664
baden.i.ua@gmail.com <baden.i.ua@gmail.com@72927f16-9f08-a1fb-f96f-45abbbfd52dd>
2011-02-06 10:58:14 +0000

    Add new externals Add forgoted cron and database flushing.

--- trunk/js/.compiller/get.externals.bat
+++ trunk/js/.compiller/get.externals.bat
@@ -1,7 +1,9 @@
 SET curl=D:\Programs\Git\bin\curl.exe
 SET svn=D:\Programs\mingw\svn-win32-1.6.2\bin\svn.exe
 
-rem %curl% http://closure-compiler.googlecode.com/files/compiler-latest.zip --output compiler.jar
+%curl% http://closure-compiler.googlecode.com/files/compiler-latest.zip --output compiler.jar
+
+exit
 
 rem jquery-1.4.4.js
 for %%i in (jquery-1.5.js, google_loader_api.js, webkit_console.js, maps/google_maps_api_v3_3.js) do (
